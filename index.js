3f7bb7737a04f854c6f0da59bb5656f61eec617c
SanjaiSadhasivam <sanjaisnj97@gmail.com>
2024-06-28 17:00:06 +0530

    notification

--- index.js
+++ index.js
@@ -25,8 +25,12 @@ const io = new Server(http, {
 //{"userId" : "socket ID"}
 
 const userSocketMap = {};
-
+let unreadMessages = {};
+let activeUsers = [];
 io.on("connection", (socket) => {
+  socket.on("me", (id) => {
+    socket.join(id);
+  });
   console.log("a user is connected", socket.id);
 
   const userId = socket.handshake.query.userId;
@@ -37,25 +41,60 @@ io.on("connection", (socket) => {
     userSocketMap[userId] = socket.id;
   }
 
-  console.log("user socket data", userSocketMap);
+  socket.on("me", (id) => {
+    socket.join(id);
+    if (!activeUsers.includes(id) && id !== null && id !== "null") {
+      activeUsers.push(id);
+    }
+    io.emit(
+      "activeUsers",
+      activeUsers.filter((i) => i !== "")
+    );
+  });
+
+  console.log(activeUsers, "onlineStatus");
   socket.on("join", (roomid) => {
     socket.join(roomid);
   });
+  socket.on("removeUser", (userId) => {
+    console.log("user disconnected successs", userId);
+    const userStatus = activeUsers.filter((i) => i !== userId);
+    activeUsers = userStatus.filter((i) => i !== "");
+    console.log(activeUsers, "removedUser");
+    io.emit("activeUsers", activeUsers);
+  });
   socket.on("sendMessage", ({ senderId, receiverId, messages, roomid }) => {
-    console.log(roomid, "msg");
-    const receiverSocketId = userSocketMap[receiverId];
+    // socket.to(roomid).emit("notification", messages);
 
-    console.log("receiver Id", receiverId);
+    io.to(receiverId).emit("notification", messages);
 
     io.to(roomid).emit("newMessage", {
       senderId,
       messages,
     });
-    // if (receiverSocketId) {
+
+    if (receiverId in unreadMessages) {
+      unreadMessages[receiverId]++;
+    } else {
+      unreadMessages[receiverId] = 1;
+    }
+
+    // Emit the updated unread message count to the receiver
+    io.to(receiverId).emit("unreadMessageCount", {
+      to: receiverId,
+      count: unreadMessages[receiverId],
+    });
+
     socket.on("disconnect", () => {
       console.log("user disconnected", socket.id);
-      delete userSocketMap[userId];
     });
     // }
+  });
+  socket.on("markMessagesAsRead", (receiverId) => {
+    unreadMessages[receiverId] = 0;
+    io.to(receiverId).emit("unreadMessageCount", {
+      to: receiverId,
+      count: 0,
+    });
   });
 });
